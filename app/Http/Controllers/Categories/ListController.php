34905471127f02ce5cf8505fd116a930cdf0d42e
Steve Harrison <steve@harricoder.com>
2023-04-30 16:24:01 +0100

    categories: more cs fixes

--- app/Http/Controllers/Categories/ListController.php
+++ app/Http/Controllers/Categories/ListController.php
@@ -13,6 +13,7 @@ class ListController extends Controller
 {
     /**
      * Handle the incoming request.
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
     public function __invoke(Request $request): View
     {
